e1ef2ac63790bf9ef569b0c9a55d743db6a4e8ee
Sanikatsitp <sanikaphadatre510@gmail.com>
2026-03-26 12:06:11 +0530

    Implement if-else structure to evaluate x
    i am just doing an trial learning of github,on how github works,never really used it.

---
+++ if-else.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int x;
+cout<<"Enter the value of x: ";
+cin>>x;
+if(x>0){
+cout<<"x is greater than 0";
+}else if(x<0){
+cout<<"x is less than 0";
+}
+  else{
+    cout<<"x is not zero";
+  }
+return 0;
+}
